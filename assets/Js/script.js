96953bdc5e9a35fbb01b20d5a3002fbaa28f2c36
AjabaGodswill <AjabaGodswill@gmail.com>
2021-10-28 11:30:42 +0100

    Addede HTML5 geolocation API

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,17 +1,30 @@
-"use strict"
-const name = document.getElementById("name")
-const password = document.getElementById("password")
-const form = document.getElementById("form")
-const errorElement = document.getElementById("error")
+// "use strict"
+// const name = document.getElementsByClassName("UserName")
+// const password = document.getElementsByClassName("password")
+// const form = document.getElementsByClassName("form")
+// const email = document.getElementsByClassName("email")
 
-form.addEventListener("submit", (e) => {
-    let messages = [];
-    if (name.value === "" || name.value === null) {
-        messages.push("Name is required")
-    }
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerHTML = messages.join(',')
+// const errorElement = document.getElementsByClassName("error")
 
+// form.addEventListener('submit', (e) => {
+//     let messages = [];
+//     if (name.value === "" || name.value === null) {
+//         messages.push("Name is required")
+//     }
+//     if (messages.length > 0) {
+//         e.preventDefault()
+//         errorElement.innerHTML = messages.join(',')
+
+// }
+// })
+const successCallback = (position) => {
+    console.log(position);
 }
-})
+
+const errorCallback = (error) => {
+    console.error(error);
+}
+
+const UserLocation = () => {
+    navigator.geolocation.getCurrentLocation(successCallback, errorCallback)
+}
